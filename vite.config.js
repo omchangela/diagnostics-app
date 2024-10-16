0e88e1c559bc95e947319579df6cd1ec2ccf76c2
kali-om <kali-om@kali>
2024-10-16 15:31:19 +0530

    some updates

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true, // Ensure that the server serves index.html for all routes
+    historyApiFallback: true,
   },
 })
